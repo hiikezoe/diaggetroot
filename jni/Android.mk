193e7ae12a67c046193a14f23fa2a2abd4151aec
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-01-08 20:20:00 +0900

    Remove CR.

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= \
 	jni.c \
-	diag.c 
+	diag.c
 LOCAL_CFLAGS += -std=c99
 LOCAL_MODULE := diaggetroot
 LOCAL_LDLIBS := -llog
@@ -12,7 +12,7 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= \
 	jni.c \
-	diag.c 
+	diag.c
 LOCAL_CFLAGS += -std=c99
 LOCAL_MODULE := diaggetrootjni
 LOCAL_LDLIBS := -llog
